684a162493eaa1f4db64f7bda9e9e623db8a4b40
Kevin Castejon <castejonkevin@gmail.com>
2025-10-06 02:24:57 +0200

    add WeightedTiles sample

--- Samples~/Shared Assets/Scripts/Runtime/ATile.cs
+++ Samples~/Shared Assets/Scripts/Runtime/ATile.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using Caskev.GridToolkit;
 namespace GridToolkitWorkingProject.Demos.Shared
 {
-public abstract class ATile : MonoBehaviour, ITile
-{
-    private bool _isWalkable;
-    private int _x;
-    private int _y;
+    public abstract class ATile : MonoBehaviour, ITile
+    {
+        private bool _isWalkable;
+        private int _x;
+        private int _y;
 
-    public bool IsWalkable { get => _isWalkable; set => _isWalkable = value; }
-    public int X { get => _x; set => _x = value; }
-    public int Y { get => _y; set => _y = value; }
-}
+        public bool IsWalkable { get => _isWalkable; set => _isWalkable = value; }
+        public int X { get => _x; set => _x = value; }
+        public int Y { get => _y; set => _y = value; }
+    }
 }
